b2e35882ddbf779b19aee112c568db157b7d89b2
patrykpalka <patryk-palka2@wp.pl>
2025-05-28 19:49:02 +0200

    feat: update Java version to 17 and add Retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,16 +26,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
